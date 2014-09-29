a7a9db6109a1cf432cb3b229175f89122bec49ca
Ladislav Mizenko <laci@adaptiware.com>
2014-09-30 00:14:34 +0200

    Added script for ultrasonic sensor HC-SR04;

---
+++ ultrasonic/hc-sr04.py
@@ -0,0 +1,50 @@
+import time
+import RPi.GPIO as GPIO
+
+def measure():
+    GPIO.output(GPIO_TRIGGER, True)
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    start = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 0:
+        start = time.time()
+    
+    while GPIO.input(GPIO_ECHO) == 1:
+        stop = time.time()
+
+    elapsed = stop - start
+    distance = (elapsed * 34300) / 2
+
+    return distance
+
+def measure_safe():
+    time.sleep(0.1)
+    return measure()
+
+def measure_average():
+    distances = [measure_safe() for x in range(5)]
+    distance = sum(distances) / len(distances)
+    return distance
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
+
+print('Ultrasonic measurement')
+
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+GPIO.output(GPIO_TRIGGER, False)
+
+try:
+    while True:
+        distance = measure_average()
+        print('Distance: %.1f' % distance)
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+    
